d330c59d0fd42a1bcb7622fef9aca026957a0d29
tzc123 <774061539@qq.com>
2018-02-07 21:11:27 +0800

    change path

--- webhook/deploy.js
+++ webhook/deploy.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const createHandler = require('github-webhook-handler')
 
-const handler = createHandler({ path: '/webhook', secret: '129315' })
+const handler = createHandler({ path: '/', secret: '129315' })
 const server = http.createServer((req, res) => {
   handler(req, res, function (err) {
     res.statusCode = 404
