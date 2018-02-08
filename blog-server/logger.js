efbcca0e50b945127a1d76da919eb891956f509d
tzc123 <774061539@qq.com>
2018-02-08 19:53:30 +0800

    ../..

--- blog-server/logger.js
+++ blog-server/logger.js
@@ -4,13 +4,6 @@ require('winston-daily-rotate-file')
 
 const { Logger, transports: { DailyRotateFile, File } } = winston
 
-const infoDailyRotateFile = new DailyRotateFile({
-  filename: path.join(__dirname, `logs/.info.log`),
-  datePattern: 'yyyy_MM_dd',
-  prepend: true,
-  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info'
-})
-
 module.exports = new Logger({
     transports: [
       new DailyRotateFile({
