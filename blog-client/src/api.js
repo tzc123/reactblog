04a59eee541c6c88dae254f747361237d0e7fe9e
tzc123 <774061539@qq.com>
2018-02-26 15:50:04 +0800

    ../..

--- blog-client/src/api.js
+++ blog-client/src/api.js
@@ -1,7 +1,7 @@
 import { get, post } from './utils/request'
-const domain = process.env.DEV == 'remote'
-               ? 'http://122.152.205.25:4321'
-               : 'http://localhost:4321'
+const domain = process.env.DEV == 'local'
+               ? 'http://localhost:4321'
+               : 'http://122.152.205.25:4321'
 
 export function getArticleList(category, index) {
   return get(
