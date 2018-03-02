4149765d5841b140a360a5dfc03bdb5c76cf25a4
tzc123 <774061539@qq.com>
2018-03-02 21:45:45 +0800

    ../..

--- blog-server/controllers/article.js
+++ blog-server/controllers/article.js
@@ -11,10 +11,7 @@ module.exports = {
       request: { url, method } } = ctx
     try{
       let articles = await getArticles(category, size, index)
-      console.log(articles)
       const browses = await getBrowses()
-      console.log(browses)
-      
       articles = articles.map(article => {
         const browse = browses[article._id] || 0
         return {
