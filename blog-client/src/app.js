2a589c5cda52ba5b2e209b71be3ad4839a515435
tzc123 <774061539@qq.com>
2018-03-05 22:55:51 +0800

    ../..

--- blog-client/src/app.js
+++ blog-client/src/app.js
@@ -1,7 +1,12 @@
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 import RouterComponent from './router.js'
 import './styles/base.css'
+const renderMethod = window.initialData
+	? ReactDOM.hydrate
+	: module.hot
+		? ReactDOM.render 
+		: ReactDOM.hydrate
 
-ReactDom.hydrate(
+renderMethod(
 	<RouterComponent />, document.getElementById('app')
 )
